e584436866cacc1dae16ecb43702d866bb75b4fe
Khushboo <khusbhoosingh1322@gmail.com>
2026-04-29 22:54:55 +0530

    Fix home route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import CreatePost from './pages/CreatePost.jsx'
 import Feed from './pages/Feed.jsx'
+
 const App = () => {
   return (
     <Router>
-        <Routes>
-              <Route path='/Create-Post' element={
-                <CreatePost />}/>
-
-                      <Route path='/feed' element={<Feed />} /> 
-          </Routes>
-      </Router>
+      <Routes>
+        <Route path='/' element={<Navigate to="/feed" />} />
+        <Route path='/create-post' element={<CreatePost />} />
+        <Route path='/feed' element={<Feed />} />
+      </Routes>
+    </Router>
   )
 }
 
